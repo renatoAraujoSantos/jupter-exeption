c7a80f55edef5823ba07ab629b9ea76db3724656
Renato Santos <renato.colossus@gmail.com>
2021-11-25 22:25:23 -0300

    react redirect

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "assets/scss/blk-design-system-react.scss?v=1.2.0";
-import ErroStripe from 'components/ErroStripe.js';
+import ErroStripe from './components/ErroStripe';
 
 ReactDOM.render(
     <BrowserRouter>
@@ -33,3 +14,4 @@ ReactDOM.render(
 
     document.getElementById('root')
 );
+
