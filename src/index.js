3787990c6ceb3b30694ed9ba31a01cead9e7f327
Renato Santos <renato.colossus@gmail.com>
2021-11-25 21:53:05 -0300

    jupter exeption

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import "assets/scss/blk-design-system-react.scss?v=1.2.0";
+import ErroStripe from 'components/ErroStripe.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <BrowserRouter>
+        <Switch>
+            <Route path="/redirect" render={(props) => <ErroStripe {...props} />} />
+            <Redirect from="/" to="/redirect" />
+        </Switch>
+    </BrowserRouter>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    document.getElementById('root')
+);
